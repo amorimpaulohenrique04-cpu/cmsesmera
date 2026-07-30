0d847ac3aed03445e14275f387fe707075a37b2b
amorimpaulohenrique04-cpu <amorimpaulohenrique04@gmail.com>
2026-07-30 20:37:14 -0300

    Adiciona tema visual Esméra ao Studio

---
+++ studio/esmeraTheme.ts
@@ -0,0 +1,23 @@
+import {buildLegacyTheme} from 'sanity'
+
+export const esmeraTheme = buildLegacyTheme({
+  '--black': '#111210',
+  '--white': '#F7F5EF',
+  '--gray': '#7B7972',
+  '--gray-base': '#7B7972',
+  '--component-bg': '#F7F5EF',
+  '--component-text-color': '#111210',
+  '--brand-primary': '#1B4B40',
+  '--default-button-color': '#6F716C',
+  '--default-button-primary-color': '#1B4B40',
+  '--default-button-success-color': '#356C59',
+  '--default-button-warning-color': '#A07D48',
+  '--default-button-danger-color': '#8B3D3D',
+  '--state-info-color': '#4C7068',
+  '--state-success-color': '#356C59',
+  '--state-warning-color': '#A07D48',
+  '--state-danger-color': '#8B3D3D',
+  '--main-navigation-color': '#173E35',
+  '--main-navigation-color--inverted': '#F3F0E8',
+  '--focus-color': '#2E6A5B',
+})

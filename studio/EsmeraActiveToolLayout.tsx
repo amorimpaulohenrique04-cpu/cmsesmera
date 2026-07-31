3c27c359c352e3da7279d0e2737054e86913cbd7
amorimpaulohenrique04-cpu <amorimpaulohenrique04@gmail.com>
2026-07-30 23:54:56 -0300

    Use single-pane Stitch application layout

--- studio/EsmeraActiveToolLayout.tsx
+++ studio/EsmeraActiveToolLayout.tsx
@@ -1,98 +1,26 @@
+import {useEffect} from 'react'
 import {type ActiveToolLayoutProps} from 'sanity'
 import styled from 'styled-components'
 import {esmeraTokens as t} from './esmeraTokens'
 
-const ActiveToolFrame = styled.div`
+const Frame = styled.div`
   position: relative;
   min-height: 0;
   height: 100%;
-  background: ${t.color.canvas};
+  background: ${t.color.surface};
 
   &[data-esmera-tool='cms'] {
-    [data-ui='Pane'] {
-      border-color: ${t.color.line} !important;
-      background: ${t.color.surface};
-    }
-
-    [data-ui='Pane']:first-of-type {
-      flex: 0 0 ${t.layout.sidebar}px !important;
-      width: ${t.layout.sidebar}px !important;
-      max-width: ${t.layout.sidebar}px !important;
-      background: ${t.color.surface};
-    }
-
-    [data-ui='Pane']:first-of-type nav {
-      padding-right: 12px;
-      padding-left: 12px;
-    }
-
-    [data-ui='Pane']:first-of-type nav [data-ui='Button'],
-    [data-ui='Pane']:first-of-type nav [role='button'] {
-      min-height: 44px;
-      border-radius: ${t.radius.navItem}px !important;
-      box-shadow: none !important;
-      transition:
-        background-color ${t.motion.fast} ${t.motion.easing},
-        color ${t.motion.fast} ${t.motion.easing};
-    }
-
-    [data-ui='Pane']:first-of-type nav [data-ui='Button']:hover,
-    [data-ui='Pane']:first-of-type nav [role='button']:hover {
-      background: rgba(73, 107, 89, 0.07) !important;
-      transform: none !important;
-    }
-
-    [data-ui='Pane']:first-of-type nav [data-ui='Button'][data-selected='true'],
-    [data-ui='Pane']:first-of-type nav [data-ui='Button'][aria-selected='true'],
-    [data-ui='Pane']:first-of-type nav [data-ui='Button'][aria-current='page'],
-    [data-ui='Pane']:first-of-type nav [role='button'][data-selected='true'],
-    [data-ui='Pane']:first-of-type nav [role='button'][aria-selected='true'],
-    [data-ui='Pane']:first-of-type nav [role='button'][aria-current='page'] {
-      background: ${t.color.emeraldSoft} !important;
-      color: ${t.color.ink} !important;
-    }
-
-    [data-ui='Pane']:first-of-type nav svg {
-      width: 18px;
-      height: 18px;
-    }
-
-    [data-ui='Pane']:first-of-type hr {
-      margin-top: 10px;
-      margin-bottom: 10px;
-      border-color: ${t.color.line};
-    }
-  }
-
-  @media (max-width: 1439px) and (min-width: 1200px) {
-    &[data-esmera-tool='cms'] [data-ui='Pane']:first-of-type {
-      flex-basis: 240px !important;
-      width: 240px !important;
-      max-width: 240px !important;
-    }
-  }
-
-  @media (max-width: 1199px) and (min-width: 1024px) {
-    &[data-esmera-tool='cms'] [data-ui='Pane']:first-of-type {
-      flex-basis: 216px !important;
-      width: 216px !important;
-      max-width: 216px !important;
-    }
-  }
-
-  @media (max-width: 1023px) {
-    &[data-esmera-tool='cms'] [data-ui='Pane']:first-of-type {
-      width: auto !important;
-      max-width: none !important;
-      flex-basis: auto !important;
-    }
+    [data-ui='Pane'] { border-color: ${t.color.line} !important; background: ${t.color.surface}; }
+    [data-ui='Pane']:not(:last-of-type) { display: none !important; }
+    [data-ui='Pane']:last-of-type { flex: 1 1 100% !important; width: 100% !important; max-width: none !important; }
   }
 `
-
 export function EsmeraActiveToolLayout(props: ActiveToolLayoutProps) {
-  return (
-    <ActiveToolFrame data-esmera-tool={props.activeTool.name}>
-      {props.renderDefault(props)}
-    </ActiveToolFrame>
-  )
+  useEffect(() => {
+    if (props.activeTool.name !== 'cms' || typeof window === 'undefined') return
+    const path = window.location.pathname.replace(/\/$/, '')
+    if (path === '/site/cms') window.location.replace('/site/cms/dashboard')
+    if (path === '/business/cms') window.location.replace('/business/cms/customers')
+  }, [props.activeTool.name])
+  return <Frame data-esmera-tool={props.activeTool.name}>{props.renderDefault(props)}</Frame>
 }

fd5d0ff43ce775260d2a10ced5d9c6171255b6da
amorimpaulohenrique04-cpu <amorimpaulohenrique04@gmail.com>
2026-07-31 00:33:07 -0300

    Remove pane-hiding workaround from CMS tool layout

--- studio/EsmeraActiveToolLayout.tsx
+++ studio/EsmeraActiveToolLayout.tsx
@@ -1,4 +1,3 @@
-import {useEffect} from 'react'
 import {type ActiveToolLayoutProps} from 'sanity'
 import styled from 'styled-components'
 import {esmeraTokens as t} from './esmeraTokens'
@@ -8,19 +7,8 @@ const Frame = styled.div`
   min-height: 0;
   height: 100%;
   background: ${t.color.surface};
-
-  &[data-esmera-tool='cms'] {
-    [data-ui='Pane'] { border-color: ${t.color.line} !important; background: ${t.color.surface}; }
-    [data-ui='Pane']:not(:last-of-type) { display: none !important; }
-    [data-ui='Pane']:last-of-type { flex: 1 1 100% !important; width: 100% !important; max-width: none !important; }
-  }
 `
+
 export function EsmeraActiveToolLayout(props: ActiveToolLayoutProps) {
-  useEffect(() => {
-    if (props.activeTool.name !== 'cms' || typeof window === 'undefined') return
-    const path = window.location.pathname.replace(/\/$/, '')
-    if (path === '/site/cms') window.location.replace('/site/cms/dashboard')
-    if (path === '/business/cms') window.location.replace('/business/cms/customers')
-  }, [props.activeTool.name])
   return <Frame data-esmera-tool={props.activeTool.name}>{props.renderDefault(props)}</Frame>
 }
